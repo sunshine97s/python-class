67d1a9f6b0199f0e0d10146d60fd1412142ad93e
sunshine97s <41152634+sunshine97s@users.noreply.github.com>
2018-07-13 12:26:48 -0500

    exercise 7
    exercise 7

--- exercise6.py
+++ exercise6.py
@@ -1,7 +1,3 @@
-#length=len(word)
-#print("the input = " + word)
-#print("the output of length number = " + word[length::-1])
-#print("the output of reversed word is " +  word[::-1])
 word = input("Please input a word: ")
 a = word[::-1]
 print(a)
